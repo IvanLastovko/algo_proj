861b560500e1e232e93f6a76030386e6f2b1eea9
IvanLastovko <ivan.lastovko@ericsson.com>
2022-01-21 08:58:05 +0200

    second commit

--- src/components/Tutorial.js
+++ src/components/Tutorial.js
@@ -9,7 +9,7 @@ export const Tutorial = ({tutorialOpen, setTutorialOpen}) => {
         return (<div className={'tutorial_box'} onClick={() => setTutorialOpen(false)}>
                 <div className={'tutorial_text'} onClick={() => setTutorialOpen(true)}>
                     <br></br>
-                    <h1>What's RegEx?</h1>
+                    <h1>What's RegEx? <sup><a href={'https://en.wikipedia.org/wiki/Regular_expression'}>[1]</a></sup></h1>
                     <p className={'p'}>Regular expression is a sequence of characters that specifies a search pattern in
                         text. Usually such patterns are used by string-searching algorithms for "find" or "find and
                         replace"
@@ -42,7 +42,7 @@ export const Tutorial = ({tutorialOpen, setTutorialOpen}) => {
                         members. In most formalisms, if there exists at least one regular expression that matches a
                         particular set then there exists an infinite number of other regular expressions that also match
                         it.</p>
-                    <h1>Nondeterministic finite automaton</h1>
+                    <h1>Nondeterministic finite automaton <sup><a href={'https://en.wikipedia.org/wiki/Nondeterministic_finite_automaton'}>[2]</a></sup></h1>
                     <p className={'p'}>A nondeterministic finite automaton (NFA), or nondeterministic finite-state
                         machine, does not need to obey restrictions of deterministic finite automaton (DFA). In
                         particular, every DFA is also an NFA.</p>
@@ -57,7 +57,7 @@ export const Tutorial = ({tutorialOpen, setTutorialOpen}) => {
                         following a different transition. If no transition is applicable, the current copy is in a dead
                         end, and it "dies". If, after consuming the complete input, any of the copies is in an accept
                         state, the input is accepted, else, it is rejected.</p>
-                    <h1>Thompson's construction algorithm</h1>
+                    <h1>Thompson's construction algorithm <sup><a href={'https://en.wikipedia.org/wiki/Thompson%27s_construction'}>[3]</a></sup></h1>
                     <p className={'p'}>Thompson's construction algorithm is a method of transforming a regular
                         expression
                         into an equivalent nondeterministic finite automaton (NFA). The algorithm works recursively by
@@ -70,7 +70,7 @@ export const Tutorial = ({tutorialOpen, setTutorialOpen}) => {
                     <p className={'p'}>The purple oval corresponds to <code>a</code>, the teal oval corresponds
                         to <code>a*</code>, the green oval corresponds to <code>b</code>, the orange oval corresponds
                         to <code>a*b</code>, and the blue oval corresponds to <code>ε</code>.</p>
-                    <h1>Deterministic finite automaton</h1>
+                    <h1>Deterministic finite automaton <sup><a href={'https://en.wikipedia.org/wiki/Deterministic_finite_automaton'}>[4]</a></sup></h1>
                     <p className={'p'}>In automata theory, a finite-state machine is called a deterministic finite
                         automaton (DFA), if:</p>
                     <p className={'p'}><b>—</b> each of its transitions is uniquely determined by its source state and
@@ -81,7 +81,7 @@ export const Tutorial = ({tutorialOpen, setTutorialOpen}) => {
                         which for each state and symbol, the transition function has exactly one state. Thus, it is
                         clear that every formal language that can be recognized by a DFA can be recognized by a NFA.</p>
                     <p className={'p'}></p>
-                    <h1>DFA minimization</h1>
+                    <h1>DFA minimization <sup><a href={'https://en.wikipedia.org/wiki/DFA_minimization'}>[5]</a></sup></h1>
                     <p className={'p'}>For each regular language, there also exists a minimal automaton that accepts it,
                         that is, a DFA with a minimum number of states and this DFA is unique (except that states can be
                         given different names). The minimal DFA ensures minimal computational cost for tasks such
